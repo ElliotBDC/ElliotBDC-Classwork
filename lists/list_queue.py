9515bd13d1007b7f21a994cf99aa0323886f0814
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2022-11-06 12:57:58 +0000

    Feedback: Queues
    Okay, but you need to be doing a bit more, look at the comments in the code.

--- lists/list_queue.py
+++ lists/list_queue.py
@@ -1,3 +1,8 @@
+### SRC - A good attempt, but you needed to return the popped value
+### and you need to make sure you identify your functions and procedures
+### in the pseudocode comments. I would expect you to have more test cases 
+### to show this is working correctly.
+
 queue = []
 size = 5
 def enQueue(value):
@@ -6,13 +11,16 @@ def enQueue(value):
     else:
         return "Full"
     #endif
+#end procedure
 
 def deQueue(value=None):
     if value == None:
-        queue.pop()
+        queue.pop() ### You need to retuen the popped value
     else:
         queue.pop(queue.index(value))
-        #endif
+    #endif
+#end function
+    
 
 def isEmpty():
     if len(queue) != 0:
