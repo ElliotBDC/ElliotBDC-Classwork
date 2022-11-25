9ea323ad7ffba490d5a7369cc6d37a055e6a62d3
ElliotBDC <35963237+ElliotBDC@users.noreply.github.com>
2022-11-25 13:09:26 +0000

    Create palindrome(Stacks).py

---
+++ stacks/palindrome(Stacks).py
@@ -0,0 +1,24 @@
+myString = input("Enter word: ")
+stack = []
+s = ""
+for c in myString:
+    if 64 < ord(c) < 91:
+        s = s + c
+    stack.append(c)
+    #end if
+#next c
+
+newString = ""
+for _ in range(len(stack)):
+    newString = newString + stack.pop()
+#next _
+
+if myString == newString:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+#end if
+
+if myString == myString[::-1]:
+    print("True")
+#end if
