0ea7e7b9ece9822cd39037afa18d55718e4e16c3
ElliotBDC <35963237+ElliotBDC@users.noreply.github.com>
2023-01-08 14:15:15 +0000

    Create 1a.py

---
+++ arrays/1a.py
@@ -0,0 +1,10 @@
+numbers = [0 for i in range(6)] #Creating an array  List acts as an array.
+for index in range(6): 
+    numbers[i] = (int(input("Enter number: "))) 
+avg = 0 
+for i in range(len(numbers)): 
+    avg = avg + i 
+    print(numbers[-i-1]) 
+print(f"Average: {avg/len(numbers)}") 
+
+#Note: instead of using I for for loops, if looping through loops etc, name the variable index instead to make it more clear.
