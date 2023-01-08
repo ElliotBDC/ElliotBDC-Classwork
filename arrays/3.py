e355e2eaac7737e4278e028f3ceb9e76cba820f8
ElliotBDC <35963237+ElliotBDC@users.noreply.github.com>
2023-01-08 15:37:22 +0000

    Create 3.py

---
+++ arrays/3.py
@@ -0,0 +1,6 @@
+Outlet1Sales = [10, 12,15,10]
+Outlet2Sales = [5,8,3,6]
+Outlet3Sales = [10,12,15,10]
+for index in range(1, 5):
+    print(f"Total for quarter '{index}': {Outlet1Sales[index-1] + Outlet2Sales[index-1] + Outlet3Sales[index-1]}")
+#next n
